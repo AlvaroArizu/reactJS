8ad75d0a061e60b47da0687cd92d7f893951c684
AlvaroArizu <alvaro.arizu2000@gmail.com>
2024-02-02 18:19:36 -0300

    Agregue estados, tp M2 U1

--- Modulo 1/Unidad 3 /Js/Archivo/src/Detalle.jsx
+++ Modulo 1/Unidad 3 /Js/Archivo/src/Detalle.jsx
@@ -1,8 +1,28 @@
+import React, { useState } from 'react';
+
 function Detalle() {
+  // Estado para manejar la visibilidad del mensaje
+  const [mostrarMensaje, setMostrarMensaje] = useState(false);
+
+  // Función para manejar el clic en el botón "Comprar"
+  const handleCompraClick = () => {
+    // Cambiar el estado para mostrar el mensaje
+    setMostrarMensaje(true);
+  };
+
   return (
     <div>
-      <p>nombre: Moto g</p>
-      <p>precio: $1000</p>
+      <p>Nombre: Moto G</p>
+      <p>Descripción: Smartphone de última generación.</p>
+      <p>Precio: $1000</p>
+      <p>SKU: SKU123456</p>
+      <p>Cantidad disponible: 10</p>
+
+      {/* Botón "Comprar" con evento onClick */}
+      <button onClick={handleCompraClick}>Comprar</button>
+
+      {/* Mostrar el mensaje si mostrarMensaje es verdadero */}
+      {mostrarMensaje && <p>Gracias por su compra</p>}
     </div>
   );
 }
